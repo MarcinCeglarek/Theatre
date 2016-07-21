678c944d49a90d5196970281dfab5b2cb6a6435e
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2016-07-21 12:40:49 +0100

    Need wrapper for file and directory operations

--- Theatre.Common/Theatre.Common.Tests/Agents/DirectoryReaderTests.cs
+++ Theatre.Common/Theatre.Common.Tests/Agents/DirectoryReaderTests.cs
@@ -1,8 +1,55 @@
-﻿using Akka.TestKit.VsTest;
+﻿using System.IO;
+using Akka.Actor;
+using Akka.TestKit;
+using Akka.TestKit.VsTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Theatre.Common.Agents;
+using Theatre.Common.Messages;
 
 namespace Theatre.Common.Tests.Agents
 {
+    [TestClass]
     public class DirectoryReaderTests : TestKit
     {
+        private readonly string nonExistentPath = "non/existent/path";
+        private string _existentPath;
+
+        private IActorRef _target;
+        private TestProbe _testProbe;
+
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            _existentPath = Directory.GetCurrentDirectory();
+            _testProbe = this.CreateTestProbe();
+            _target = ActorOf(Props.Create(() => new DirectoryReader(_testProbe)));
+        }
+
+        [TestMethod]
+        public void StartsProcessingDirectory()
+        {
+            EventFilter.Info("Processing " + nonExistentPath)
+                .ExpectOne(() => _target.Tell(new HashDirectory(nonExistentPath)));
+        }
+
+        [TestMethod]
+        public void LogsWarningIfDirectoryDoesNotExists()
+        {
+            EventFilter.Warning(nonExistentPath + ": Dir not found")
+                .ExpectOne(() => _target.Tell(new HashDirectory(nonExistentPath)));
+        }
+
+        [TestMethod]
+        public void LogsInfoWhenStartingProcessingDirectory()
+        {
+            EventFilter.Info("Reading " + _existentPath).ExpectOne(() => _target.Tell(new HashDirectory(_existentPath)));
+        }
+
+        [TestMethod]
+        public void SendsMessageWhenFinishedReadingDirectory()
+        {
+            _target.Tell(new HashDirectory(_existentPath));
+            _testProbe.ExpectMsg<DirectoryHashed>(message => message.FullPath == _existentPath);
+        }
     }
 }
--- Theatre.Common/Theatre.Common/Agents/DirectoryReader.cs
+++ Theatre.Common/Theatre.Common/Agents/DirectoryReader.cs
@@ -1,8 +1,41 @@
-﻿using Akka.Actor;
+﻿using System.IO;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Event;
+using Theatre.Common.Messages;
 
 namespace Theatre.Common.Agents
 {
-    public class DirectoryReader : TypedActor
+    public class DirectoryReader : ReceiveActor
     {
+        private readonly IActorRef _databaser;
+        private readonly ILoggingAdapter _logger;
+
+        public DirectoryReader(IActorRef databaser)
+        {
+            _databaser = databaser;
+            _logger = Context.GetLogger();
+            ReceiveAsync<HashDirectory>(message => ProcessDirectory(message.FullPath));
+        }
+
+        private Task ProcessDirectory(string fullPath)
+        {
+            this._logger.Info("Processing " + fullPath);
+            if (Directory.Exists(fullPath))
+            {
+                this._logger.Info("Reading " + fullPath);
+
+                ProcessChildrenDirectories();
+                ProcessChildrenFiles();
+
+                _databaser.Tell(new DirectoryHashed(fullPath));
+            }
+            else
+            {
+                this._logger.Warning(fullPath + ": Dir not found");
+            }
+
+            return Task.FromResult(0);
+        }
     }
 }
